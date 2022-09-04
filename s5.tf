8cfe85644b768dff55647c3613da581f9f1ecbb6
AbdulFattah666 <112809472+AbdulFattah666@users.noreply.github.com>
2022-09-04 15:26:01 +0300

    s5

---
+++ s5.tf
@@ -0,0 +1,17 @@
+resource "aws_sns_topic" "s5example1" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
+resource "aws_sns_topic" "s5example2" {
+}
+
+resource "aws_sns_topic" "s5example3" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
